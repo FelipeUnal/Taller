074ae05d573fac512f39ce929f36bd854fd64752
FelipeUnal <fsescalantet@unal.edu.co>
2017-02-08 00:46:41 -0500

    Agregado

--- Taller/src/taller/Taller.java
+++ Taller/src/taller/Taller.java
@@ -12,20 +12,20 @@ package taller;
 public class Taller {
     
     static double Punto1(double a, double b){
-        double x= a*1000*b;
-        double r= x/(2*3.14);
-        return (r*r)*3.14; 
+        double x = (a/0.001)*b; //Circunferencia, unidades en metros
+        double r = ((x/2)/3.141592)/1000;//Radio, Conversion a Km
+        return ((r*r)*3.141592); 
     }
     
     public static void main(String[] args) {
         
         java.util.Scanner teclado = new java.util.Scanner(System.in);
-        double a= teclado.nextDouble();
-        double b= teclado.nextDouble();
-        double c= Punto1(a,b);
-        
         System.out.println("Ejercicio 1");
         System.out.println("Ingresa los valores");
+        System.out.println(" ");
+        double a = teclado.nextDouble();
+        double b = teclado.nextDouble();
+        double c = Punto1(a,b);
         System.out.println(" ");
         System.out.println(c);
         
