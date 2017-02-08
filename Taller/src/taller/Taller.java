d537f093ede8007cbfb86abedb389acc574dc6b4
FelipeUnal <fsescalantet@unal.edu.co>
2017-02-08 00:11:01 -0500

    Agregado

--- Taller/src/taller/Taller.java
+++ Taller/src/taller/Taller.java
@@ -10,12 +10,25 @@ package taller;
  * @author Estudiante
  */
 public class Taller {
-
-    /**
-     * @param args the command line arguments
-     */
+    
+    static double Punto1(double a, double b){
+        double x= a*1000*b;
+        double r= x/(2*3.14);
+        return (r*r)*3.14; 
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        java.util.Scanner teclado = new java.util.Scanner(System.in);
+        double a= teclado.nextDouble();
+        double b= teclado.nextDouble();
+        double c= Punto1(a,b);
+        
+        System.out.println("Ejercicio 1");
+        System.out.println("Ingresa los valores");
+        System.out.println(" ");
+        System.out.println(c);
+        
     }
     
 }
