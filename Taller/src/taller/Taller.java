df1786337bdb88b29a2cae32e087d88c4d010ab2
FelipeUnal <fsescalantet@unal.edu.co>
2017-02-08 01:17:33 -0500

    Agregado

--- Taller/src/taller/Taller.java
+++ Taller/src/taller/Taller.java
@@ -17,17 +17,31 @@ public class Taller {
         return ((r*r)*3.141592); 
     }
     
+    static void Punto2(int a){
+        int z[];
+        z = new int[a];
+    } 
+    
     public static void main(String[] args) {
         
         java.util.Scanner teclado = new java.util.Scanner(System.in);
         System.out.println("Ejercicio 1");
-        System.out.println("Ingresa los valores");
-        System.out.println(" ");
+        System.out.println("Ingresa los valores :");
         double a = teclado.nextDouble();
         double b = teclado.nextDouble();
         double c = Punto1(a,b);
-        System.out.println(" ");
+        System.out.print("El area del Circulo es: ");
         System.out.println(Math.floor(c));
+        
+        System.out.println(" ");
+        System.out.println("Ejercicio 2 ");
+        System.out.print("Ingresa el numero de Histogramas: ");
+        int z = teclado.nextInt();
+        System.out.println(z);
+        System.out.println(" ");
+        
+        
+        
         
     }
     
