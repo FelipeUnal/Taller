747d7d0dd10220d29a287c8aa446cfe4a3567bbf
FelipeUnal <fsescalantet@unal.edu.co>
2017-03-03 19:01:07 -0500

    Agregado

--- Taller/src/taller/Taller.java
+++ Taller/src/taller/Taller.java
@@ -1,6 +1,8 @@
 
 package taller;
 
+import java.io.PrintStream;
+
 
 public class Taller {
     
@@ -82,6 +84,41 @@ public class Taller {
         return n;
     }
     
+    static int Ejercicio2_C(){
+        int nD = 0;
+        int n = 0;
+        for(int i = 1; i<=100;i++){
+            int ConD = 0;
+            for(int j = 1; j<=i; j++){
+                if(i%j == 0){ConD++;}
+            }
+            if(ConD>=nD){
+                nD = ConD;
+                n = i;
+            }
+        }
+        System.out.println("El numero "+n+" tiene "+nD+" divisores.");
+        return n;
+    }
+    
+    static void Permutacion(){
+        int i;
+        int[] a = new int[4];
+        for(i = 0; i<=4; i++){
+            a[i] = i+1;
+        }
+        permutacion(a,4);
+    }
+    
+    static void Ejercicio2_D(){
+        int n = 4; 
+        int k = 0;      
+        int t = 0;
+        for(int i = 1; i>n; i++){
+            
+        }
+    }
+        
     public static void main(String[] args) {
         
         java.util.Scanner teclado = new java.util.Scanner(System.in);
@@ -114,6 +151,7 @@ public class Taller {
         
         System.out.println(" ");
         System.out.println("Punto 2-A");
+        System.out.println("Escribe un numero");
         int na1;
         na1 = teclado.nextInt();
         System.out.println(Ejercicio2_A(na1));
@@ -123,10 +161,22 @@ public class Taller {
         System.out.println(" ");
         int nb2;
         System.out.println("Punto 2-B");
+        System.out.println("Escribe un numero");
         nb2 = teclado.nextInt();
         System.out.println(Ejercicio2_B(nb2));
         System.out.println("Imprime asteriscos en diagonal.");
         System.out.println("La primera diagonal contiene la mitad de la segunda diagonal."); 
         System.out.println("La segunda diagonal contiene los asteriscos del numero marcado.");
+        
+        System.out.println(" ");
+        System.out.println("Punto 2-C");
+        System.out.println(Ejercicio2_C());
+        System.out.println("Es una funcion que indica la cantidad de divisores que tiene el numero 96.");
+        
+        
+    }
+
+    private static void permutacion(int[] a, int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
